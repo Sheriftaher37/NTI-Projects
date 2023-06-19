b025126d471d2dde6d9c673e4cdd1d041da25db1
Sheriftaher37 <129824364+Sheriftaher37@users.noreply.github.com>
2023-06-20 00:49:40 +0300

    Add files via upload

---
+++ STEPPER MOTOR/Debug/STEPPER_program.d
@@ -0,0 +1,17 @@
+STEPPER_program.o STEPPER_program.o: ../STEPPER_program.c ../STD_TYPES.h \
+  ../BIT_MATH.h ../DIO_interface.h ../PORT_interface.h \
+  ../STEPPER_interface.h ../STEPPER_config.h ../STEPPER_private.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../DIO_interface.h:
+
+../PORT_interface.h:
+
+../STEPPER_interface.h:
+
+../STEPPER_config.h:
+
+../STEPPER_private.h:
