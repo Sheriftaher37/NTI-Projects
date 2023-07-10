e0dabe7f6c64df907fdd95e036e78f22c173b07e
Sheriftaher37 <129824364+Sheriftaher37@users.noreply.github.com>
2023-07-10 10:19:13 +0300

    Add files via upload

---
+++ WATER_LEVELS/Debug/LED_program.d
@@ -0,0 +1,17 @@
+LED_program.o LED_program.o: ../LED_program.c ../STD_TYPES.h \
+  ../BIT_MATH.h ../DIO_register.h ../DIO_interface.h ../LED_interface.h \
+  ../LED_config.h ../LED_private.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../DIO_register.h:
+
+../DIO_interface.h:
+
+../LED_interface.h:
+
+../LED_config.h:
+
+../LED_private.h:
---
+++ WATER_LEVELS/main.c
@@ -0,0 +1,16 @@
+#include "STD_TYPES.h"
+#include "DIO_interface.h"
+#include "PORT_interface.h"
+#include "ADC_interface.h"
+#include "LED_interface.h"
+
+int main(void)
+{
+	PORT_voidInit();
+	ADC_voidInit();
+
+	while(1)
+	{
+
+	}
+}
